6f80729119342833c2c580f55d8eb266083611be
LeeNux <lc7457@qq.com>
2023-03-15 12:55:00 +0800

    增加图片跨域

--- src/libs/loader.js
+++ src/libs/loader.js
@@ -2,6 +2,7 @@ import {fabric} from "fabric";
 
 export function loadFromURL(src, callback) {
     fabric.Image.fromURL(src, img => {
+        img.setAttribute('crossOrigin', 'anonymous');
         callback && callback(img)
     })
 }
